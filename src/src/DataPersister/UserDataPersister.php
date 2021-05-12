aeb13c1a1ea0263c5b1e2664bc59fbbcb8e94a6c
Akred <akred@live.com>
2021-05-12 16:00:06 +0200

    Make generic date updater datapersister

--- src/src/DataPersister/UserDataPersister.php
+++ src/src/DataPersister/UserDataPersister.php
@@ -2,44 +2,14 @@
 
 namespace App\DataPersister;
 
-use ApiPlatform\Core\DataPersister\DataPersisterInterface;
 use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 
-class UserDataPersister implements DataPersisterInterface
+class UserDataPersister extends UpdateDateDataPersister
 {
-
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
 
     public function supports($data): bool
     {
         return $data instanceof User;
     }
 
-    /**
-     * @param User $data
-     *
-     * @return void
-     */
-    public function persist($data)
-    {
-        if (!$data->getId()) {
-            $data->setCreatedAt(new \DateTime('now'));
-        }
-        $data->setUpdatedAt(new \DateTime('now'));
-
-        $this->entityManager->persist($data);
-        $this->entityManager->flush();
-    }
-
-    public function remove($data)
-    {
-        $this->entityManager->remove($data);
-        $this->entityManager->flush();
-    }
 }
